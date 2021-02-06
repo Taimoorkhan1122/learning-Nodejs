c5732840f756a87532706520fd19037ee98ca85b
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-02-06 21:05:22 +0500

    Feat: serving static file using node js

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const http = require("http");
+const static = require("node-static");
+
+const hostname = "127.0.0.1";
+const port = 3500;
+
+// const { URL } = require("url");
+// const myUrl = new URL("/home", "http://localhost:3500");
+
+const file = new static.Server("./code");
 
 const requestListner = (req, res) => {
-  res.writeHead(200);
-  res.end("Hello World from Node server");
+  file.serve(req, res);
+  console.log(req.url);
+  // if (req.url === "/home") {
+  //   req
+  //     .addListener("end", function () {
+  //       file.serve(req, res);
+  //     })
+  //     .resume();
+  // }
+  // } else if (req.url === "/") {
+  //   res.writeHead(200);
+  //   res.end("Hello from node server");
+  // } else {
+  //   res.writeHead(404);
+  //   // res.end("no rout found");
+  // }
 };
 
 const app = http.createServer(requestListner);
 
-app.listen(3500);
+app.listen(port, hostname, () => {
+  console.log(`server is up on PORT: ${port}`);
+});
